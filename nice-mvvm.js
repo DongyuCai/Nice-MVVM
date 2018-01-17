ea9cd3442af379c10b94be187577d2bf87d6b4e1
DongyuCai <1157656909@qq.com>
2018-01-17 16:50:50 +0800

    新增nc-src指令。

--- nice-mvvm.js
+++ nice-mvvm.js
@@ -1,6 +1,7 @@
 'use strict';
 //支持指令：
 //1.nc-value  只能写变量，不可以写表达式，双向绑定，凡是有value属性的元素，都可以使用。
+//4.nc-src	  例 nc-src="{{basePictureUrl+item.picture}}"
 //2.nc-for	  只能写命令，row in ary 这样的形式，任何元素都可以使用，有一点注意，只能使用基本属性，不能传递entity，比如onclick="delete({{row}})"，这是不行的，但是可以onclick="delete({{$index}})"。
 //3.nc-if	  支持表达式
 //（已放弃nc-class）4.nc-class  支持指令，express?class1:class2这样的形式，express可以是表达式
@@ -184,7 +185,23 @@ var $NICE_MVVM = function(mvvmElement){
 					});
 				}
 			},
-			'nc-for':{
+            'nc-src':{
+                'commandName':'nc-src',//绑定src属性，必须要求有nc-src值，可以避免原生html的src因为表达式出现网络404的问题。
+                'initFunc':function(node,proPath){
+                    var node_nc_id = $SCOPE.$NODE_ID_POINT++;
+
+                    //加入到V2M_大Map里
+                    $SCOPE.$ADD_V2M_NODE_MAP(proPath,{
+                        'id':node_nc_id,
+                        'node':node,
+                        'expression':proPath,
+                        'render':function(proPath,val){
+                            this.node.src=val;
+                        }
+                    });
+                }
+            },
+            'nc-for':{
 				'commandName':'nc-for',//for循环
 				'initFunc':function(node,command){
 					var node_nc_id = $SCOPE.$NODE_ID_POINT++;
